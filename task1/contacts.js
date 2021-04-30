faab0ca6f06066fc37e076f60507817ffb41d128
AleksandrKolot <64203565+AleksandrKolot@users.noreply.github.com>
2021-04-30 14:24:18 +0300

    task2_2

--- task1/contacts.js
+++ task1/contacts.js
@@ -1,19 +1,42 @@
 import fs from "fs";
 import path from "path";
 
-const contactPath = path.join("./db", "contacts.json");
-console.log(contactPath);
+// const contactPath = path.join("./db", "contacts.json");
+// console.log(contactPath);
+const contactPath = path.resolve("db/contacts.json");
 
-function listContacts() {
-  fs.readFile(`${contactPath}`, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
+async function getContacts() {
+  try {
+    const result = await fs.readFile(contactPath, "utf8");
+    const contacts = JSON.parse(result);
+    return contacts;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function listContacts() {
+  try {
+    const contacts = await getContacts();
+    contacts.forEach((contact) => {
+      console.log(contact.name);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 console.log(listContacts());
+
+// function listContacts() {
+//   fs.readFile(`${contactPath}`, "utf8", (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(data);
+//   });
+// }
+// console.log(listContacts());
 
 // function getContactById(contactId) {
 //   fs.readFile(`${contactPath}`, "utf8", (err, data) => {
