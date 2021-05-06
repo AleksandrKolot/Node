ad7e4746ad8a4599646c1ceec8daeb5207ed9873
Aleksandr Kolot <aleksandrkolot7@gmail.com>
2021-05-06 21:02:34 +0300

    task2_3_contacts

--- task1/contacts.js
+++ task1/contacts.js
@@ -1,6 +1,7 @@
-import fs from "fs";
-import { get } from "http";
+import fs from "fs/promises";
+// import { get } from "http";
 import path from "path";
+import { v4 as generateId } from "uuid";
 
 const contactPath = path.join("./db", "contacts.json");
 // console.log(contactPath);
@@ -33,6 +34,39 @@ export async function getContactById(contactId) {
     const contacts = await getContacts();
     const requiredContact = contacts.filter(({ id }) => id === contactId);
     console.log(requiredContact);
+  } catch (err) {
+    console.error(err);
+  }
+}
+export async function removeContact(contactId) {
+  try {
+    const contacts = await getContacts();
+    if (contact.find(({ id }) => id === contactId)) {
+      const updatedContactList = contacts.filter(({ id }) => id !== contactId);
+      await fswriteFile(contsctsPath, JSON.stringify(updatedContactList));
+      console.log(`contct with id ${contactId} was deleted`);
+      listContacts();
+      return;
+    }
+    console.log(`no contact with this ${contactId}`);
+    listContacts();
+  } catch (err) {
+    console.error(err);
+  }
+}
+export async function addContact(name, email, phone) {
+  try {
+    const contscts = await getContactso();
+    const newContact = {
+      id: generateId(),
+      name,
+      email,
+      phone,
+    };
+    const updatedContactList = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContsctList));
+    console.log(`contact ${name} was added`);
+    listContacts();
   } catch (err) {
     console.error(err);
   }
